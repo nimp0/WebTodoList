8f39f5ae5b237294f55181f09c7ebb5e0ace62eb
Nimpo <cloud_nimpo@ukr.net>
2019-06-10 22:00:51 +0300

    added adding elements/ fixed deleting

--- main.js
+++ main.js
@@ -1,37 +1,133 @@
 let form = document.getElementById("form");
 let close = document.getElementsByClassName("delete");
 let itemList = document.getElementById("taskList");
-let button = document.getElementsByClassName("button");
-let item = document.getElementsByClassName("item");
 
-// Click on a close button to hide the current list item
-for (let i = 0; i < close.length; i++) {
-  close[i].addEventListener("click", function(e) {
-    let currentElement = this.parentElement.parentElement;
-    itemList.removeChild(currentElement);
-  });
-}
-
-// Create a new list item when clicking on the "Submit" button
 form.addEventListener("submit", createNewElement);
+itemList.addEventListener("click", RemoveElement);
+// Create Item
+
 function createNewElement(e) {
   e.preventDefault();
 
   let name = document.querySelector(".input.name").value;
   let date = document.querySelector(".input.date").value;
   let task = document.querySelector(".input.task").value;
-  console.log(task);
 
-  let li = document.createElement("li");
-  li.className = "items";
+  let items = document.createElement("li");
+  items.className = "items";
+  itemList.appendChild(items);
 
-  let nameField = document.createElement("div");
-  nameField.className = "labelInfoTaskList name";
-  nameField.value = name;
-  console.log(nameField.value);
-  nameField.appendChild(document.createTextNode(name));
-  //item.appendChild(document.createTextNode(name));
+  let infoArea = document.createElement("div");
+  infoArea.className = "infoArea";
+  items.appendChild(infoArea);
 
-  li.appendChild(document.createTextNode(name));
-  itemList.appendChild(li);
+  let optionArea = document.createElement("div");
+  optionArea.className = "optionArea";
+  items.appendChild(optionArea);
+
+  //InfoArea
+  let listName = document.createElement("div");
+  let listDate = document.createElement("div");
+  let listTask = document.createElement("div");
+  listName.className = "labelTaskList";
+  listDate.className = "labelTaskList";
+  listTask.className = "labelTaskList";
+  listName.appendChild(document.createTextNode("Name"));
+  listDate.appendChild(document.createTextNode("Date"));
+  listTask.appendChild(document.createTextNode("Task"));
+
+  let listNameArea = document.createElement("div");
+  let listDateArea = document.createElement("div");
+  let listTaskArea = document.createElement("div");
+  listNameArea.className = "labelInfoTaskList";
+  listDateArea.className = "labelInfoTaskList";
+  listTaskArea.className = "labelInfoTaskList";
+  listNameArea.appendChild(document.createTextNode(name));
+  listDateArea.appendChild(document.createTextNode(date));
+  listTaskArea.appendChild(document.createTextNode(task));
+
+  let infoItems = [];
+  for (let i = 0; i < 3; i++) {
+    infoItems[i] = document.createElement("div");
+    infoItems[i].className = "item";
+    infoArea.appendChild(infoItems[i]);
+
+    if (i == 0) {
+      infoItems[i].appendChild(listName);
+      infoItems[i].appendChild(listNameArea);
+    }
+    if (i == 1) {
+      infoItems[1].appendChild(listDate);
+      infoItems[1].appendChild(listDateArea);
+    }
+    if (i == 2) {
+      infoItems[2].appendChild(listTask);
+      infoItems[2].appendChild(listTaskArea);
+    }
+  }
+
+  //Option Area
+  let status = document.createElement("button");
+  status.className = "status";
+  status.appendChild(document.createTextNode("⇓"));
+  optionArea.appendChild(status);
+
+  let del = document.createElement("button");
+  del.className = "delete";
+  del.appendChild(document.createTextNode("X"));
+  console.log(del);
+  console.log(close);
+  optionArea.appendChild(del);
+  arr.push(del);
 }
+
+//Remove Item
+
+function RemoveElement(e) {
+  if (e.target.classList.contains("delete")) {
+    var li = e.target.parentElement.parentElement;
+    itemList.removeChild(li);
+  }
+}
+
+/*const rootOfFile = document.querySelector("#root");
+
+interface TodoItem: {
+  name: string;
+  task: string;
+  date: string;
+}
+
+const todoItemList = [];
+
+function addTodo(name, task, date) {
+  todoItemList.push({ name, task, date });
+}
+
+function initialize() {
+  addTodo("demo", "fix", "2018-08-08");
+}
+
+function renderTodo(todoItem) {
+  const div = document.createElement("div");
+  const str = `Title: ${todoItem.name}, ${todoItem.task}, ${todoItem.date}`;
+
+  const text = document.createTextNode(str);
+  div.appendChild(text);
+  div.appendChild(document.createElement("br"));
+
+  return div;
+}
+
+function renderAdd() {}
+
+function renderAll() {
+  for (let k in todoItemList) {
+    const v = todoItemList[k];
+    const todo = renderTodo(v);
+    root.appendChild(todo);
+  }
+}
+
+initialize();
+renderAll();*/
